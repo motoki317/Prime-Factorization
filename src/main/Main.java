aaae1f3f2a6bec4b35fcac2de69553124edc6a94
motoki317 <motoki317@gmail.com>
2019-04-30 15:21:54 +0900

    Added InputUtils to ask for specific input / Modified Main to ask for beginning and end number

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,17 @@
 package main;
 
+import util.InputUtils;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        long start = System.nanoTime();
+        InputUtils input = new InputUtils();
 
-        long beginning = 1000000000L; // inclusive
-        long limit = beginning + 2; // exclusive
+        int beginning = input.askIntInput("Input the beginning number");
+        int limit = beginning + input.askIntInput("Input how many numbers to continue factorizing.");
+
+        long start = System.nanoTime();
 
         for (long i = beginning; i < limit; i++) {
             PrimeFact f = new PrimeFact(i);
@@ -20,4 +24,5 @@ public class Main {
         System.out.println("Took " + (end - start)/1000000D + " ms to compute.");
 
     }
+
 }
